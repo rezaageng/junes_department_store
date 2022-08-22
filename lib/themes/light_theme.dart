1351d34e0fcec7f56b4a70f1a6420120a41d2bac
Reza Ageng <rezaageng5@gmail.com>
2022-08-22 14:28:11 +0700

    style(theme): :lipstick: change subtitle2 style

--- lib/themes/light_theme.dart
+++ lib/themes/light_theme.dart
@@ -19,7 +19,7 @@ ThemeData lightTheme = ThemeData(
   textTheme: const TextTheme(
     headline6: TextStyle(color: Colors.black),
     caption: TextStyle(height: 1.5),
-    subtitle2: TextStyle(color: Colors.white70),
+    subtitle2: TextStyle(color: Colors.black54),
   ),
   fontFamily: 'NotoSansJP',
   textSelectionTheme: const TextSelectionThemeData(
