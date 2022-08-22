74c5336b98cea9a88daa1289f6578f52a41a1f74
Reza Ageng <rezaageng5@gmail.com>
2022-08-22 14:35:57 +0700

    feat(home): :sparkles: add product item

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../screens/product_details.dart';
+
 class ProductItem extends StatelessWidget {
   final String title;
   final String image;
@@ -13,41 +15,51 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(16),
-      child: GridTile(
-        header: Material(
-          color: Colors.transparent,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              IconButton(
-                splashRadius: 24,
-                icon: const Icon(Icons.favorite_outline),
-                onPressed: () {},
+      child: Material(
+        child: GestureDetector(
+          onTap: () =>
+              Navigator.of(context).pushNamed(ProductDetails.routeName),
+          child: GridTile(
+            header: Material(
+              color: Colors.transparent,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                    enableFeedback: false,
+                    splashRadius: 24,
+                    icon: const Icon(Icons.favorite_outline),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    enableFeedback: false,
+                    splashRadius: 24,
+                    icon: const Icon(Icons.shopping_cart_outlined),
+                    color: Colors.white,
+                    onPressed: () {},
+                  ),
+                ],
               ),
-              IconButton(
-                splashRadius: 24,
-                icon: const Icon(Icons.shopping_cart_outlined),
-                onPressed: () {},
+            ),
+            footer: GridTileBar(
+              backgroundColor: Theme.of(context).cardTheme.color,
+              title: Text(
+                title,
+                style: Theme.of(context).textTheme.bodyMedium,
               ),
-            ],
-          ),
-        ),
-        footer: GridTileBar(
-          backgroundColor: Theme.of(context).cardTheme.color,
-          title: Text(
-            title,
-            textAlign: TextAlign.center,
-          ),
-          subtitle: Text(
-            '\$${price.toStringAsFixed(0)}',
-            // textAlign: TextAlign.center,
-          ),
-        ),
-        child: Container(
-          color: Theme.of(context).cardTheme.color,
-          child: Image.network(
-            image,
-            fit: BoxFit.cover,
+              subtitle: Text(
+                '\$${price.toStringAsFixed(0)}',
+                style: Theme.of(context).textTheme.subtitle2,
+              ),
+            ),
+            child: Container(
+              color: Theme.of(context).cardTheme.color,
+              child: Image.network(
+                image,
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
         ),
       ),
