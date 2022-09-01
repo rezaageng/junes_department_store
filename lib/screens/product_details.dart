7bd71f48c60b72de3e9bb77547a9b0b5567a78cf
Reza Ageng <rezaageng5@gmail.com>
2022-09-01 12:19:33 +0700

    feat(product details): :sparkles: add product details data

--- lib/screens/product_details.dart
+++ lib/screens/product_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:junes_department_store/providers/cart.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/product.dart';
@@ -15,13 +16,105 @@ class ProductDetails extends StatelessWidget {
         ModalRoute.of(context)!.settings.arguments as String;
     final Product product =
         Provider.of<Products>(context, listen: false).findById(productId);
+    final Cart cart = Provider.of<Cart>(context, listen: false);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
       ),
       body: Center(
-        child: Text(product.description),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                physics: const BouncingScrollPhysics(),
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 8),
+                    child: AspectRatio(
+                      aspectRatio: 1,
+                      child: Image.network(
+                        product.image,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          product.title,
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        Text(
+                          '\$${product.price.toString()}',
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5!
+                              .copyWith(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    child: Text(
+                      product.description,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: product.toggleFavorite,
+                      style: ElevatedButton.styleFrom(
+                        primary: Theme.of(context).colorScheme.error,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.favorite_rounded),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text('Favorite')
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () => cart.addItem(
+                          product.id, product.title, product.price),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.shopping_cart_rounded),
+                          SizedBox(
+                            width: 8,
+                          ),
+                          Text('Add to cart')
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
