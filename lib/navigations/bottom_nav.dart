59314f010de2c9dd60d9ba4a971ebf7f70264e98
Reza Ageng <rezaageng5@gmail.com>
2022-09-09 11:05:05 +0700

    feat(navigations): :sparkles: add pop handler

--- lib/navigations/bottom_nav.dart
+++ lib/navigations/bottom_nav.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../screens/home.dart';
 import '../screens/orders_screen.dart';
@@ -26,42 +27,59 @@ class _BottomNavState extends State<BottomNav> {
 
   int _screenIndex = 0;
 
+  Future<bool> _onWillPop() async {
+    if (_screenIndex == 0) {
+      await SystemNavigator.pop();
+    }
+
+    Future.delayed(const Duration(milliseconds: 0), () {
+      setState(() {
+        _screenIndex = 0;
+      });
+    });
+
+    return _screenIndex == 0;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: const Icon(Icons.menu_rounded),
-            onPressed: () => Scaffold.of(context).openDrawer(),
-            enableFeedback: false,
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Builder(
+            builder: (context) => IconButton(
+              icon: const Icon(Icons.menu_rounded),
+              onPressed: () => Scaffold.of(context).openDrawer(),
+              enableFeedback: false,
+            ),
           ),
+          title: Text(_screens[_screenIndex]['title'] as String),
+          actions: const [CartButton()],
         ),
-        title: Text(_screens[_screenIndex]['title'] as String),
-        actions: const [CartButton()],
-      ),
-      drawer: const AppDrawer(),
-      body: _screens[_screenIndex]['screen'] as Widget,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _screenIndex,
-        onTap: (index) => setState(() {
-          _screenIndex = index;
-        }),
-        enableFeedback: false,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home_rounded),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.receipt_outlined),
-            activeIcon: Icon(Icons.receipt_rounded),
-            label: 'Orders',
-          ),
-        ],
+        drawer: const AppDrawer(),
+        body: _screens[_screenIndex]['screen'] as Widget,
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _screenIndex,
+          onTap: (index) => setState(() {
+            _screenIndex = index;
+          }),
+          enableFeedback: false,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              activeIcon: Icon(Icons.home_rounded),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.receipt_outlined),
+              activeIcon: Icon(Icons.receipt_rounded),
+              label: 'Orders',
+            ),
+          ],
+        ),
       ),
     );
   }
