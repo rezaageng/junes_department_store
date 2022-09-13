b4ec6fe7c88756ab716faad3e2d28782a5ff0325
Reza Ageng <rezaageng5@gmail.com>
2022-09-13 13:37:11 +0700

    feat(home): :sparkles: add init state from firebase

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -6,43 +6,7 @@ import 'package:http/http.dart' as http;
 import 'product.dart';
 
 class Products with ChangeNotifier {
-  final List<Product> _items = [
-    Product(
-      id: 'p1',
-      title: 'Television',
-      description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Ornare massa eget egestas purus viverra. Id semper risus in hendrerit gravida rutrum quisque non. Consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Malesuada fames ac turpis egestas maecenas pharetra. Ultrices dui sapien eget mi proin sed. Imperdiet nulla malesuada pellentesque elit eget gravida. Interdum consectetur libero id faucibus. Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Urna nunc id cursus metus aliquam eleifend mi in. Justo donec enim diam vulputate ut pharetra sit. In ornare quam viverra orci sagittis eu volutpat odio facilisis.',
-      price: 120,
-      image:
-          'https://cdnb.artstation.com/p/assets/images/images/029/446/953/large/antonio-mg-3.jpg?1597588635',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Kuma',
-      description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Ornare massa eget egestas purus viverra. Id semper risus in hendrerit gravida rutrum quisque non. Consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Malesuada fames ac turpis egestas maecenas pharetra. Ultrices dui sapien eget mi proin sed. Imperdiet nulla malesuada pellentesque elit eget gravida. Interdum consectetur libero id faucibus. Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Urna nunc id cursus metus aliquam eleifend mi in. Justo donec enim diam vulputate ut pharetra sit. In ornare quam viverra orci sagittis eu volutpat odio facilisis.',
-      price: 25,
-      image:
-          'https://static.wikia.nocookie.net/megamitensei/images/5/59/PQ2_Teddie.png/revision/latest?cb=20180901221207',
-    ),
-    Product(
-      id: 'p3',
-      title: 'Scooter',
-      description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Ornare massa eget egestas purus viverra. Id semper risus in hendrerit gravida rutrum quisque non. Consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Malesuada fames ac turpis egestas maecenas pharetra. Ultrices dui sapien eget mi proin sed. Imperdiet nulla malesuada pellentesque elit eget gravida. Interdum consectetur libero id faucibus. Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Urna nunc id cursus metus aliquam eleifend mi in. Justo donec enim diam vulputate ut pharetra sit. In ornare quam viverra orci sagittis eu volutpat odio facilisis.',
-      price: 1500,
-      image:
-          'https://assets.reedpopcdn.com/persona-4-golden-social-stats-increase-knowledge-diligence-courage-expression-understanding-7035-1593446229177.jpg/BROK/thumbnail/1200x1200/quality/100/persona-4-golden-social-stats-increase-knowledge-diligence-courage-expression-understanding-7035-1593446229177.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: 'School Uniform',
-      description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. A condimentum vitae sapien pellentesque habitant morbi tristique senectus. Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Ornare massa eget egestas purus viverra. Id semper risus in hendrerit gravida rutrum quisque non. Consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Nec feugiat in fermentum posuere urna nec tincidunt praesent. Malesuada fames ac turpis egestas maecenas pharetra. Ultrices dui sapien eget mi proin sed. Imperdiet nulla malesuada pellentesque elit eget gravida. Interdum consectetur libero id faucibus. Aliquam purus sit amet luctus venenatis lectus magna fringilla urna. Urna nunc id cursus metus aliquam eleifend mi in. Justo donec enim diam vulputate ut pharetra sit. In ornare quam viverra orci sagittis eu volutpat odio facilisis.',
-      price: 24,
-      image: 'https://st.cdjapan.co.jp/pictures/l/00/44/NEOGDS-4181_3.jpg?v=1',
-    )
-  ];
+  List<Product> _items = [];
 
   List<Product> get items => [..._items];
 
@@ -50,6 +14,35 @@ class Products with ChangeNotifier {
       _items.where((item) => item.isFavorite).toList();
 
   Product findById(id) => _items.firstWhere((item) => item.id == id);
+
+  Future<void> fetchProduct() async {
+    final url = Uri.https(
+      'junes-departement-store-default-rtdb.asia-southeast1.firebasedatabase.app',
+      '/products.json',
+    );
+
+    try {
+      final response = await http.get(url);
+      final products = jsonDecode(response.body) as Map<String, dynamic>;
+      final List<Product> loadedProduct = [];
+
+      products.forEach((id, product) {
+        loadedProduct.add(Product(
+          id: id,
+          title: product['title'],
+          description: product['description'],
+          price: product['price'],
+          image: product['image'],
+        ));
+      });
+
+      _items = loadedProduct;
+
+      notifyListeners();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 
   Future<void> addProduct(Product product) async {
     final url = Uri.https(
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -19,11 +19,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _isInit = true;
+  bool _isLoading = false;
+
   FilterOptions _showProducts = FilterOptions.all;
 
   void _changeFilter(FilterOptions filter) => setState(() {
         _showProducts = filter;
       });
+
+  @override
+  void didChangeDependencies() async {
+    super.didChangeDependencies();
+    if (_isInit) {
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<Products>(context).fetchProduct();
+      setState(() {
+        _isLoading = false;
+      });
+    }
+    _isInit = false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,47 +51,52 @@ class _HomeState extends State<Home> {
             ? products.favoriteProduct
             : products.items;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(16),
-          child: Row(
+    return _isLoading
+        ? const Center(
+            child: CircularProgressIndicator.adaptive(),
+          )
+        : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Filter(
-                title: 'All',
-                onTap: () => _changeFilter(FilterOptions.all),
-              ),
-              const SizedBox(width: 12),
-              Filter(
-                  title: 'Favorite',
-                  onTap: () => _changeFilter(FilterOptions.favorite))
-            ],
-          ),
-        ),
-        Expanded(
-          child: filteredProducts.isEmpty
-              ? const Center(
-                  child: Text('Nothing here :('),
-                )
-              : GridView.builder(
-                  padding: const EdgeInsets.all(16),
-                  physics: const BouncingScrollPhysics(),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: 2 / 3,
-                    crossAxisSpacing: 16,
-                    mainAxisSpacing: 16,
-                  ),
-                  itemCount: filteredProducts.length,
-                  itemBuilder: ((context, index) =>
-                      ChangeNotifierProvider.value(
-                        value: filteredProducts[index],
-                        child: const ProductItem(),
-                      )),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Filter(
+                      title: 'All',
+                      onTap: () => _changeFilter(FilterOptions.all),
+                    ),
+                    const SizedBox(width: 12),
+                    Filter(
+                        title: 'Favorite',
+                        onTap: () => _changeFilter(FilterOptions.favorite))
+                  ],
                 ),
-        ),
-      ],
-    );
+              ),
+              Expanded(
+                child: filteredProducts.isEmpty
+                    ? const Center(
+                        child: Text('Nothing here :('),
+                      )
+                    : GridView.builder(
+                        padding: const EdgeInsets.all(16),
+                        physics: const BouncingScrollPhysics(),
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 2 / 3,
+                          crossAxisSpacing: 16,
+                          mainAxisSpacing: 16,
+                        ),
+                        itemCount: filteredProducts.length,
+                        itemBuilder: ((context, index) =>
+                            ChangeNotifierProvider.value(
+                              value: filteredProducts[index],
+                              child: const ProductItem(),
+                            )),
+                      ),
+              ),
+            ],
+          );
   }
 }
