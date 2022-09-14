9624c993c341d061db81d265dbc315d4162c8be8
Reza Ageng <rezaageng5@gmail.com>
2022-09-14 10:15:06 +0700

    feat(providers): :sparkles: add orders to firebase

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,16 +1,19 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 import 'cart.dart';
 
 class Order {
   final String id;
-  final List<CartItem> product;
+  final List<CartItem> products;
   final double amount;
   final DateTime date;
 
   Order({
     required this.id,
-    required this.product,
+    required this.products,
     required this.amount,
     required this.date,
   });
@@ -23,17 +26,43 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cart, double amount) {
-    _orders.insert(
-      0,
-      Order(
-        id: DateTime.now().toString(),
-        product: cart,
-        amount: amount,
-        date: DateTime.now(),
-      ),
+  Future<void> addOrder(List<CartItem> cart, double amount) async {
+    final url = Uri.https(
+      'junes-departement-store-default-rtdb.asia-southeast1.firebasedatabase.app',
+      '/orders.json',
     );
+    final timestamp = DateTime.now();
 
-    notifyListeners();
+    try {
+      final response = await http.post(
+        url,
+        body: jsonEncode({
+          'amount': amount,
+          'date': timestamp.toIso8601String(),
+          'products': cart
+              .map((item) => {
+                    'id': item.id,
+                    'title': item.title,
+                    'quantity': item.quantity,
+                    'price': item.price,
+                  })
+              .toList()
+        }),
+      );
+
+      _orders.insert(
+        0,
+        Order(
+          id: jsonDecode(response.body)['name'],
+          products: cart,
+          amount: amount,
+          date: timestamp,
+        ),
+      );
+
+      notifyListeners();
+    } catch (e) {
+      rethrow;
+    }
   }
 }
