475908ead26ffa7026b18b3aab813b866350acf6
Reza Ageng <rezaageng5@gmail.com>
2022-09-14 13:46:40 +0700

    refactor(orders): :recycle: use FutureBuilder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -6,54 +6,54 @@ import '../utilities/on_refresh.dart';
 import '../widgets/nothing_here.dart';
 import '../widgets/order_item.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    () async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    }();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final ordersData = Provider.of<Orders>(context, listen: false);
-
-    return _isLoading
-        ? const Center(
+    return FutureBuilder(
+      future: Provider.of<Orders>(context, listen: false).fetchOrders(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
             child: CircularProgressIndicator.adaptive(),
-          )
-        : RefreshIndicator(
-            onRefresh: () => onRefresh(
-              context,
-              () => ordersData.fetchOrders(),
-            ),
-            color: Theme.of(context).colorScheme.secondary,
-            child: ordersData.orders.isEmpty
-                ? const NothingHere()
-                : ListView.builder(
-                    physics: const BouncingScrollPhysics(
-                      parent: AlwaysScrollableScrollPhysics(),
-                    ),
-                    itemCount: ordersData.orders.length,
-                    itemBuilder: (context, index) =>
-                        OrderItem(order: ordersData.orders[index]),
-                  ),
           );
+        }
+        if (snapshot.error != null) {
+          Future.delayed(
+            Duration.zero,
+            () => ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Error fetching data'),
+              ),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.done) {
+          return Consumer<Orders>(
+            builder: (context, ordersData, child) => RefreshIndicator(
+              onRefresh: () => onRefresh(
+                context,
+                () => ordersData.fetchOrders(),
+              ),
+              color: Theme.of(context).colorScheme.secondary,
+              child: ordersData.orders.isEmpty
+                  ? const NothingHere()
+                  : ListView.builder(
+                      physics: const BouncingScrollPhysics(
+                        parent: AlwaysScrollableScrollPhysics(),
+                      ),
+                      itemCount: ordersData.orders.length,
+                      itemBuilder: (context, index) => OrderItem(
+                        order: ordersData.orders[index],
+                      ),
+                    ),
+            ),
+          );
+        } else {
+          return const NothingHere();
+        }
+      },
+    );
   }
 }
